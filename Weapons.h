51d1986bbca53d1d0e8e3160368d257eb380b66e
LevTrachuk <155105125+LevTrachuk@users.noreply.github.com>
2024-05-07 14:28:40 +0300

    Add files via upload

---
+++ Weapons.h
@@ -0,0 +1,21 @@
+#pragma once
+#include <string>
+#include "Player.h"
+#include "Enemy.h"
+
+using namespace std;
+
+class weapon {
+    friend class player;
+    friend class enemy;
+protected:
+    string name_w, type_w;
+    unsigned int damage_w, attack_speed;
+public:
+    weapon(string type, string name, unsigned int damage, unsigned int attack_speed) {
+        type_w = type;
+        name_w = name;
+        damage_w = damage;
+        this->attack_speed = attack_speed;
+    }
+};
