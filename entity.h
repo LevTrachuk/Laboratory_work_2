faf8b6ba000561940cd1d4e6e00d9fef0a4a0f75
LevTrachuk <155105125+LevTrachuk@users.noreply.github.com>
2024-05-07 14:28:24 +0300

    Add files via upload

---
+++ entity.h
@@ -0,0 +1,23 @@
+#pragma once
+#include <string>
+#include <iostream>
+#include "Player.h"
+
+using namespace std;
+
+class entity {
+    friend class player;
+protected:
+    string name_e, type_e;
+    bool is_alive;
+public:
+    entity(string name, string type, bool is_alive) {
+        name_e = name;
+        type_e = type;
+        this->is_alive = is_alive;
+    }
+
+    void Get_info() {
+        cout << "Entity type: " << type_e << "\nName: " << name_e << "\nAlive: " << is_alive;
+    }
+};
